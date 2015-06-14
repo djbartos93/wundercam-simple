c4c80432bebe1d51dceda6ecae7af95adf56423d
Eric Bartos <djbartos93@gmail.com>
2015-06-14 15:28:16 -0400

    adding some basic layout stuff

---
+++ timelapse.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#this script will create a timelapse of all the imageas from the past day (12am to 11:59pm) and then post it to a webpage
+
+ #lets start by copying the images from the past days folder to rename them to a name that ffmpg can understand
+
+foldername=$(date +%Y_%m_%d)
+cd /var/www/openmediavault/weather/cam-archive/"$foldername"
+mkdir /var/www/openmediavault/weather/timelapse/renamed-temp
+counter=1
+ls -1tr *.jpg | while read filename; do cp $filename renamed-temp/$(printf %05d $counter)_$filename; ((counter++)); done
+cd renamed-temp
+
+#now we tell ffmpeg to do its thing
+
+ffmpeg -f image2 -r 5 -i %*.jpg video.mp4
+
+#now that ffmpeg has done its things we need to move the video to a file that we can access.
+
+cp
